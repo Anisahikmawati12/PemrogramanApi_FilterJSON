bc569b8a5f98407dc71b0c95046cf9d090e4fc73
Anisahikmawati12 <134756364+Anisahikmawati12@users.noreply.github.com>
2024-10-14 20:02:47 +0700

    Add files via upload

---
+++ pemrograman_api/index.ts
@@ -0,0 +1,54 @@
+let data = [
+	{
+		id: 1,
+		name: 'John Doe',
+		hobbies: [
+			{
+				id: 1,
+				name: 'playing games',
+			},
+		],
+		age: 30,
+		address: '123 Main St',
+		city: 'New York',
+		phone: '555-1234',
+		gender: 'man',
+		religion: 'Christian',
+	},
+	{
+		id: 2,
+		name: 'Jane Smith',
+		age: 25,
+		address: '456 Oak Ave',
+		city: 'Los Angeles',
+		phone: '555-5678',
+		gender: 'man',
+		religion: 'Christian',
+
+	},
+	{
+		id: 3,
+		name: 'Michael Johnson',
+		age: 40,
+		address: '789 Pine Rd',
+		city: 'Chicago',
+		phone: '555-8765',
+		gender: 'man',
+		religion: 'Christian',
+	},
+	{
+		id: 4,
+		name: 'Emily Davis',
+		age: 35,
+		address: '321 Cedar St',
+		city: 'New York',
+		phone: '555-4321',
+		gender: 'man',
+		religion: 'Christian',
+	},
+];
+
+//TODO 1 : dapatkan semua data
+//TODO 2 : dapatkan data dengan nama tertentu
+//TODO 3 : dapatkan data dengan alamat New York
+//TODO 4 : dapatkan data dengan umur >= 30
