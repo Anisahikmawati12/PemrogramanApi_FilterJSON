e03f3ce6bf38348906768f6f9b43831769a3b823
Anisahikmawati12 <134756364+Anisahikmawati12@users.noreply.github.com>
2024-10-14 21:12:31 +0700

    Add files via upload

---
+++ sepatu.ts
@@ -0,0 +1,39 @@
+let data = [
+	{
+		"toko": {
+			"nama" : "Toko Sepatu",
+			"alamat" : "Jl.Mastrip No. 12, Blitar",
+			"nomor telepon" : "022-1287639",
+			"produk" : [
+				{
+					"id": 1,
+					"nama" : "Sepatu Olahraga",
+					"size" : "36-40",
+					"stok" : "100",
+					"harga" : "150000",
+				},
+				{
+					"id": 2,
+					"nama" : "Sepatu Flat",
+					"size" : "36-40",
+					"stok" : "150",
+					"harga" : "165000",
+				},
+				{
+					"id": 3,
+					"nama" : "Sepatu Sekolah",
+					"size" : "36-40",
+					"stok" : "80",
+					"harga" : "160000",
+				},
+				{
+					"id": 4,
+					"nama" : "Sepatu Heels",
+					"size" : "36-40",
+					"stok" : "100",
+					"harga" : "200000",
+				},
+			]
+		}
+	}
+]
